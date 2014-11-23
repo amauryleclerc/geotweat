949e64e4361943a5081e2c298baba02bf262a2ad
Amaury LECLERC <amauryleclerc@hotmail.fr>
2014-11-23 21:00:34 +0100

    ajoute openlayer 2

--- webapp/js/geotweet.js
+++ webapp/js/geotweet.js
@@ -1,69 +1,74 @@
 
-	var map;
-var vectorLayer;
+var map;
+var markersLayer;
 function dlJson() {
-	document.getElementById("search_spinner").style.display = "block";
+    effacerTweet();
+  
+    document.getElementById("search_spinner").style.display = "block";
 
-	var hashtag = document.getElementById("id_hashtag").value;
-	$.get("../server/index.php", {
-		twitter_query : "https://api.twitter.com/1.1/search/tweets.json?q="
-				+ encodeURIComponent("#TPMP")
-	}, function(data) {
-		var obj = jQuery.parseJSON(data);
-		afficherTweet(obj);
-		document.getElementById("search_spinner").style.display = "none";
-	});
+    var hashtag = document.getElementById("id_hashtag").value;
+    $.get("../server/index.php", {
+        twitter_query: "https://api.twitter.com/1.1/search/tweets.json?q="
+                + encodeURIComponent(hashtag)
+    }, function (data) {
+        var obj = jQuery.parseJSON(data);
+        afficherTweet(obj);
+        document.getElementById("search_spinner").style.display = "none";
+    });
 }
-
+function effacerTweet() {
+    var nav = document.getElementById("tweet_list");
+    nav.innerHTML = "";
+     markersLayer.clearMarkers();
+}
 function afficherTweet(data) {
-	var nav = document.getElementById("tweet_list");
+    var nav = document.getElementById("tweet_list");
 
-	for ( var i in data.statuses) {
+    for (var i in data.statuses) {
 
-		var li = document.createElement("div");
-		li.innerHTML = data.statuses[i].text ;
-		nav.appendChild(li);
-		//if(data.statuses[i].coordinates.coordinates != null ){
-			
-			ajouterMarker(data.statuses[i].coordinates.coordinates[0], data.statuses[i].coordinates.coordinates[1]);
-	//	}
-	
-	}
+        var li = document.createElement("div");
+        li.innerHTML = data.statuses[i].text;
+        nav.appendChild(li);
+        if (data.statuses[i].coordinates != null ) {
+
+          //  alert(data.statuses[i].coordinates.coordinates[0] + " " + data.statuses[i].coordinates.coordinates[1]);
+            console.log(data.statuses[i]);
+            ajouterMarker(data.statuses[i].coordinates.coordinates[0], data.statuses[i].coordinates.coordinates[1]);
+        }
+
+    }
 }
 function afficherCarte() {
-	 map = new ol.Map({
-		target : 'map',
-		layers : [ new ol.layer.Tile({
-			source : new ol.source.MapQuest({
-				layer : 'sat'
-			})
-		}) ],
-		view : new ol.View({
-			center : ol.proj.transform([ 37.41, 8.82 ], 'EPSG:4326',
-					'EPSG:3857'),
-			zoom : 4
-		})
-	});
-	  vectorLayer = new ol.layer.Vector("Overlay");
-	
-		map.addLayer(vectorLayer);
-}
-function ajouterMarker(lon, lat){
+ map = new OpenLayers.Map('map' );
+ var ol_wms = new OpenLayers.Layer.WMS(
+    "OpenLayers WMS",
+    "http://vmap0.tiles.osgeo.org/wms/vmap0",
+    {layers: "basic"}
+);
+		/*	var wmscURL = [
+				"http://wmsc1.terrapages.net/getmap?",
+				"http://wmsc2.terrapages.net/getmap?",
+				"http://wmsc3.terrapages.net/getmap?",
+				"http://wmsc4.terrapages.net/getmap?"
+			];
+			var terrapagesStreetLayer = new OpenLayers.Layer.WMS( 'TerraPages Street',wmscURL, {layers: 'UnprojectedStreet', format: 'image/jpeg' }, {buffer: 1, isBaseLayer: true} );
+		*/	map.addLayer(ol_wms);
+			map.zoomToMaxExtent();		
 
-	//markers.addMarker(new ol.Marker(new ol.LonLat(lon,lat), new ol.Icon()));
-	  var feature = new ol.Feature.Vector(
-	            new ol.geom.Point( lon, lat ).transform(epsg4326, projectTo),
-	            {description:'This is the value of<br>the description attribute'} ,
-	            {externalGraphic: './img/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
-	        );
-	/*  var iconFeature = new ol.Feature({
-		  geometry: new ol.geom.Point([0, 0]),
-		  name: 'Null Island',
-		  population: 4000,
-		  rainfall: 500
-		});*/
-	 
-	    vectorLayer.addFeatures(feature);
+ 
+ markersLayer = new OpenLayers.Layer.Markers("My Marker Layer");
+ markersLayer.setVisibility(true);
+map.addLayer(markersLayer);
+}
+function ajouterMarker(lon, lat) {
+
+var iconSize =  new OpenLayers.Size(20,20);
+var iconOffset = new OpenLayers.Pixel(-(iconSize.w/2), -iconSize.h);
+var marker = new OpenLayers.Marker(
+	new OpenLayers.LonLat(lon,lat),
+	new OpenLayers.Icon("img/twitter.png",iconSize,iconOffset)
+);
+markersLayer.addMarker(marker);
 
 }
 
