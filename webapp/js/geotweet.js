e4b92a60529036d0e1c512a7c4b6c3b69e91510c
Amaury LECLERC <amauryleclerc@hotmail.fr>
2014-11-28 11:44:24 +0100

    traitement url et hashtag + glisser nav

--- webapp/js/geotweet.js
+++ webapp/js/geotweet.js
@@ -1,10 +1,13 @@
 
 var map;
 var markersLayer;
+var projS;
+var projC;
+
 function dlJson() {
     effacerTweet();
   
-    document.getElementById("search_spinner").style.display = "block";
+    document.getElementById("search_spinner").style.display = "inline";
 
     var hashtag = document.getElementById("id_hashtag").value;
     $.get("../server/index.php", {
@@ -24,38 +27,42 @@ function effacerTweet() {
 function afficherTweet(data) {
     var nav = document.getElementById("tweet_list");
 
-    for (var i in data.statuses) {
-    if (data.statuses[i].coordinates != null ) {
-        var li = document.createElement("div");
-        li.innerHTML = data.statuses[i].text;
+    for (var i in data.statuses ) {
+if (data.statuses[i].coordinates != null && data.statuses[i].coordinates.coordinates[0] !==0 ) {
+    var text  = data.statuses[i].text;
+    text =  chercherHashtag(text) ;
+     text =  chercherURL(text);
+        var li = document.createElement("li");
+        
+        li.innerHTML = text;
         nav.appendChild(li);
-    
+        
 
-          //  alert(data.statuses[i].coordinates.coordinates[0] + " " + data.statuses[i].coordinates.coordinates[1]);
-            console.log(data.statuses[i]);
             ajouterMarker(data.statuses[i].coordinates.coordinates[0], data.statuses[i].coordinates.coordinates[1]);
         }
 
     }
 }
+function chercherURL(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+    return text.replace(exp,"<a target='_blank' href='$1'>$1</a>"); 
+}
+function chercherHashtag(text) {
+       var exp = /\S*#(\[[^\]]+\]|\S+)/g;
+    return text.replace(exp,'<a  href="#" onclick="javascript:replacerHashtag(\'$1\')" >#$1</a>'); 
+}
+function replacerHashtag(text){
+       document.getElementById("id_hashtag").value = text;
+       dlJson();
+}
+
 function afficherCarte() {
  map = new OpenLayers.Map('map' );
- var ol_wms = new OpenLayers.Layer.WMS(
-    "OpenLayers WMS",
-    "http://vmap0.tiles.osgeo.org/wms/vmap0",
-    {layers: "basic"}
-);
-		/*	var wmscURL = [
-				"http://wmsc1.terrapages.net/getmap?",
-				"http://wmsc2.terrapages.net/getmap?",
-				"http://wmsc3.terrapages.net/getmap?",
-				"http://wmsc4.terrapages.net/getmap?"
-			];
-			var terrapagesStreetLayer = new OpenLayers.Layer.WMS( 'TerraPages Street',wmscURL, {layers: 'UnprojectedStreet', format: 'image/jpeg' }, {buffer: 1, isBaseLayer: true} );
-		*/	map.addLayer(ol_wms);
-			map.zoomToMaxExtent();		
+	map.addLayer(new OpenLayers.Layer.OSM());
+map.zoomToMaxExtent();		
 
- 
+ projS = new OpenLayers.Projection("EPSG:4326");
+projC = map.getProjectionObject();
  markersLayer = new OpenLayers.Layer.Markers("My Marker Layer");
  markersLayer.setVisibility(true);
 map.addLayer(markersLayer);
@@ -65,10 +72,13 @@ function ajouterMarker(lon, lat) {
 var iconSize =  new OpenLayers.Size(20,20);
 var iconOffset = new OpenLayers.Pixel(-(iconSize.w/2), -iconSize.h);
 var marker = new OpenLayers.Marker(
-	new OpenLayers.LonLat(lon,lat),
+	new OpenLayers.LonLat(lon,lat).transform(projS, projC),
 	new OpenLayers.Icon("img/twitter.png",iconSize,iconOffset)
 );
 markersLayer.addMarker(marker);
 
+}
+function zoomMarker(id){
+    
 }
 
